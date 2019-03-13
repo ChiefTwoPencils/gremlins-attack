6c7802570d876b2a006a0fa218c913c12c8e431a
ChiefTwoPencils <robertwilk916@gmail.com>
2019-03-12 20:17:47 -0700

    Added Gremlin.NET pckg and common static imports and connected to local germlin server.

--- GremlinNet/Program.cs
+++ GremlinNet/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
+using Gremlin;
+using Gremlin.Net;
+using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Remote;
+using Gremlin.Net.Structure.IO.GraphSON;
+using static Gremlin.Net.Process.Traversal.AnonymousTraversalSource;
+using static Gremlin.Net.Process.Traversal.__;
+using static Gremlin.Net.Process.Traversal.P;
+using static Gremlin.Net.Process.Traversal.Order;
+using static Gremlin.Net.Process.Traversal.Operator;
+using static Gremlin.Net.Process.Traversal.Order;
+using static Gremlin.Net.Process.Traversal.Pop;
+using static Gremlin.Net.Process.Traversal.Scope;
+using static Gremlin.Net.Process.Traversal.TextP;
+using static Gremlin.Net.Process.Traversal.Column;
+using static Gremlin.Net.Process.Traversal.Direction;
+using static Gremlin.Net.Process.Traversal.T;
+
 namespace GremlinNet
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var g = Traversal().WithRemote(LocalRemoteConn);
+            Console.WriteLine(g.ToString());
         }
+
+        private static DriverRemoteConnection LocalRemoteConn { get; } 
+            = new DriverRemoteConnection(LocalClient);
+
+        private static GremlinClient LocalClient => new GremlinClient(
+            new GremlinServer("localhost", 8182),
+            new GraphSON3Reader(),
+            new GraphSON3Writer());
     }
 }
