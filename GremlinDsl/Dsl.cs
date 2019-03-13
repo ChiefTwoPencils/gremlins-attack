0296d7b0119fc6b319a055495330761d760e9e0e
ChiefTwoPencils <robertwilk916@gmail.com>
2019-03-13 12:04:44 -0700

    Created example with DSL.

---
+++ GremlinDsl/Dsl.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+using Gremlin;
+using Gremlin.Net;
+using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Remote;
+using Gremlin.Net.Process.Traversal;
+using Gremlin.Net.Process.Traversal.Strategy.Decoration;
+using Gremlin.Net.Structure;
+using Gremlin.Net.Structure.IO.GraphSON;
+using static Gremlin.Net.Process.Traversal.AnonymousTraversalSource;
+using static Gremlin.Net.Process.Traversal.__;
+using static Gremlin.Net.Process.Traversal.P;
+using static Gremlin.Net.Process.Traversal.Order;
+using static Gremlin.Net.Process.Traversal.Operator;
+using static Gremlin.Net.Process.Traversal.Order;
+using static Gremlin.Net.Process.Traversal.Pop;
+using static Gremlin.Net.Process.Traversal.Scope;
+using static Gremlin.Net.Process.Traversal.TextP;
+using static Gremlin.Net.Process.Traversal.Column;
+using static Gremlin.Net.Process.Traversal.Direction;
+using static Gremlin.Net.Process.Traversal.T;
+
+namespace GremlinDsl
+{
+    public static class SocialTraversalExtensions
+    {
+        public static GraphTraversal<Vertex, Vertex> Knows(this GraphTraversal<Vertex, Vertex> traversal,
+            string personName)
+        {
+            return traversal.Out("knows").HasLabel("person").Has("name", personName);
+        }
+
+        public static GraphTraversal<Vertex, int> YoungestFriendsAge(this GraphTraversal<Vertex, Vertex> traversal)
+        {
+            return traversal.Out("knows").HasLabel("person").Values<int>("age").Min<int>();
+        }
+
+        public static GraphTraversal<Vertex, long> CreatedAtLeast(this GraphTraversal<Vertex, Vertex> traversal,
+            long number)
+        {
+            return traversal.OutE("created").Count().Is(P.Gte(number));
+        }
+    }
+
+    public static class __Social
+    {
+        public static GraphTraversal<object, Vertex> Knows(string personName)
+        {
+            return __.Out("knows").HasLabel("person").Has("name", personName);
+        }
+        
+        public static GraphTraversal<object, int> YoungestFriendsAge()
+        {
+            return __.Out("knows").HasLabel("person").Values<int>("age").Min<int>();
+        }
+
+        public static GraphTraversal<object, long> CreatedAtLeast(long number)
+        {
+            return __.OutE("created").Count().Is(P.Gte(number));
+        }
+    }
+
+    public static class SocialTraversalSourceExtensions
+    {
+        public static GraphTraversal<Vertex, Vertex> Persons(this GraphTraversalSource source,
+            params string[] personNames)
+        {
+            var traversal = source.V().HasLabel("person");
+            if (personNames.Length > 0)
+            {
+                traversal = traversal.Has("name", P.Within(personNames));
+            }
+
+            return traversal;
+        }
+    }
+}
--- GremlinNet/Program.cs
+++ GremlinNet/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 using Gremlin;
 using Gremlin.Net;
 using Gremlin.Net.Driver;
 using Gremlin.Net.Driver.Remote;
+using Gremlin.Net.Process.Traversal;
+using Gremlin.Net.Process.Traversal.Strategy.Decoration;
 using Gremlin.Net.Structure.IO.GraphSON;
 using static Gremlin.Net.Process.Traversal.AnonymousTraversalSource;
 using static Gremlin.Net.Process.Traversal.__;
@@ -18,6 +21,8 @@ using static Gremlin.Net.Process.Traversal.Column;
 using static Gremlin.Net.Process.Traversal.Direction;
 using static Gremlin.Net.Process.Traversal.T;
 
+using GremlinDsl;
+
 namespace GremlinNet
 {
     class Program
@@ -25,7 +30,25 @@ namespace GremlinNet
         static void Main(string[] args)
         {
             var g = Traversal().WithRemote(LocalRemoteConn);
-            Console.WriteLine(g.ToString());
+            g = g.WithStrategies(new SubgraphStrategy(
+                HasLabel("person"), 
+                Has("weight", Gt(0.5))));
+            g.V()
+                .Values<string>("name")
+                .ToList()
+                .ToList()
+                .ForEach(Console.WriteLine);
+
+            var markoKnowsJosh = g.Persons("marko").Knows("josh");
+            Console.WriteLine(markoKnowsJosh.Next());
+            
+            var markosYoungestFriend = g.Persons("marko").YoungestFriendsAge();
+            Console.WriteLine(markosYoungestFriend.Next());
+            
+            var createdAtLeastTwo = g.Persons().CreatedAtLeast(2).Count();
+            Console.WriteLine(createdAtLeastTwo.Next());
+            
+            LocalRemoteConn.Dispose();
         }
 
         private static DriverRemoteConnection LocalRemoteConn { get; } 
