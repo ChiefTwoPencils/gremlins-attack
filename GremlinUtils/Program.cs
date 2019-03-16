58c4db358723f1f1ab451b2fa5e29a798f105d8f
ChiefTwoPencils <robertwilk916@gmail.com>
2019-03-16 13:45:38 -0700

    Added sln util proj.

---
+++ GremlinUtils/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GremlinUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
